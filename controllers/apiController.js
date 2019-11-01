8f79e4f127dd9164fe91f1e34cb08df77f9184db
Tom Hawes <twhawes@gmail.com>
2019-11-01 12:58:02 +0000

    debugged api controller

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -3,7 +3,7 @@ const endPoints = require('../endpoints.json')
 
 const getAllEndPoints = (req, res, next) => {
   res.status(200).json(endPoints)
-}.catch(next)
+}
 
 
 module.exports = { getAllEndPoints }
