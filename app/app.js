4e5c2b201b53281b102390d9cd52c97ce8db5a74
Tom Hawes <twhawes@gmail.com>
2019-11-18 14:23:01 +0000

    added cors package

--- app/app.js
+++ app/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const apiRouter = require('../routes/apiRouter.js')
 const { customErrors, psqlErrors, allOtherErrors } = require('../db/utils/errorHandling')
 const { methodNotAllowed } = require('../db/utils/errorHandling')
 
+app.use(cors())
 
 app.use(express.json());
 
